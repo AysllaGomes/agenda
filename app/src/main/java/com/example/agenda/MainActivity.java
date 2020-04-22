67d19d2f5ff74cea7875428ba921e57d98c1ca3e
ayslla.gomes <04414927010@git.economia.gov.br>
2020-04-22 16:34:54 -0300

    Implementação de uma lista de alunos, utilizando o AdpterViews para colections

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -1,9 +1,38 @@
 package com.example.agenda;
 
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+import android.os.Bundle;
+
 import android.app.Activity;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_main);
+
+        // Apenas os brabos entendem
+        List<String> alunos = new ArrayList<>(
+                Arrays.asList(
+                        "Diego Alves", "Rodrigo Caio", "Rafinha", "Filipe Luís", "Willian Arão", "Everson Ribeiro",
+                        "Diego", "De Arrascaeta", "Gabriel B.", "Pedro", "Gerson", "Michael"
+                )
+        );
+
+        ListView listaDeAlunos = findViewById(R.id.activity_main_list_de_alunos);
+
+        listaDeAlunos.setAdapter(
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos)
+        );
+    }
 
 }
